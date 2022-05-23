10888857c183ba0e08b880d9e2df64dcb9695599
Andrew.Shavulyak <savulakandrej@gmail.com>
2022-05-23 22:42:24 +0200

    added files

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string>();
+            Names.Add("Alan");
+            Names.Add("Zack");
+            Names.Add("Nick");
+            Names.Add("Andrew");
+
+            int i = 0;
+            Names = Names.Select(s => i++ % 2 != 0 ?
+            string.Join("", s.Reverse()) :
+            s).ToList();
+            foreach (var name in Names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            string[] people = { "Sam", "Dio", "Nathan", "Nicko" };
+
+            var selectedPeople = people.Select(s => s.First()).Reverse();
+
+
+            foreach (var item in selectedPeople)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+	
+    }
+}
